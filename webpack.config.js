eaa1767703f2c8f402eefa0646aa702d99f150a7
tem <artish357@gmail.com>
2023-05-14 15:28:07 -0400

    Initialize repo

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Lines and Veils Parser',
+    }),
+  ],
+};
